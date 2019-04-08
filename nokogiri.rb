4aab6fb3898d090ce44065e33300cab4a1cfe9fe
安川正幸 <yasukawamasayuki@yasupennoMacBook-Air.local>
2019-04-08 21:03:06 +0900

    add あいみょん songs

--- nokogiri.rb
+++ nokogiri.rb
@@ -1,1 +1,11 @@
-puts "hello"
+#webに接続するためのライブラリ
+require "open-uri"
+#クレイピングに使用するライブラリ
+require "nokogiri"
+ 
+doc = Nokogiri::HTML(open('https://www.uta-net.com/artist/17598/'))
+
+puts "### Search for nodes by css"
+doc.xpath('//td[@class="side td1"]').each do |node|
+  p node.css('a').text
+end
